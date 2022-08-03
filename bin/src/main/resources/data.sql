213fd86e73ae8c3c911785b1c4234a68bb1595b8
Purushottam <Pk1998.bca@gmail.com>
2022-08-03 17:43:34 +0530

    Delete bin directory

--- bin/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-insert into appuser (userid,username,password,role) values ('EMPLOYEE101','emp','emp','EMPLOYEE');
-insert into appuser (userid,username,password,role) values ('CUSTOMER101','cust','cust','CUSTOMER');
