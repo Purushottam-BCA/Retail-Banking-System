93f322627b5601b6e710de2d405732ef9aa57974
Purushottam <Pk1998.bca@gmail.com>
2022-08-03 17:44:01 +0530

    Delete target directory

--- target/classes/data.sql
+++
@@ -1,3 +0,0 @@
-insert into appuser (userid,username,password,role) values ('EMPLOYEE101','employee','employee','EMPLOYEE');
-insert into appuser (userid,username,password,role) values ('CUSTOMER101','customer','customer','CUSTOMER');
-insert into appuser (userid,username,password,role) values ('CUSTOMER102','customer','customer','CUSTOMER');
